f94046f2f606139af9841a84b6bfbe652e0bf88b
luigi055 <luigi5021@gmail.com>
2017-08-12 16:15:24 -0400

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,10 +5,19 @@ const {
 } = require('./../server');
 const Todo = require('./../models/todo');
 
+// Dummy todos (SEED DATA)
+const todos = [{
+  text: 'First test todo',
+}, {
+  text: 'Second test todo',
+}]
+
 beforeEach(done => {
   // This removes all the document todos in the collection 
-  // before each describe case
-  Todo.remove({}).then(() => done());
+  // before each 'it' case
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST / todos', () => {
@@ -28,7 +37,9 @@ describe('POST / todos', () => {
 
         // Fetch among all the todos if exist this one
         // In the mongodb
-        Todo.find().then(todos => {
+        Todo.find({
+          text
+        }).then(todos => {
           expect(todos.length).to.be.equal(1);
           expect(todos[0].text).to.be.equal(text);
           done();
@@ -45,7 +56,26 @@ describe('POST / todos', () => {
         if (err) return done(err);
 
         Todo.find().then(todos => {
-          expect(todos.length).to.be.equal(0);
+          expect(todos.length).to.be.equal(2);
+          done();
+        }).catch(err => done(err));
+      });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos created', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos).to.have.lengthOf(2);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+
+        Todo.find().then(todos => {
+          expect(todos).to.be.an('array').that.have.lengthOf(2);
           done();
         }).catch(err => done(err));
       });
