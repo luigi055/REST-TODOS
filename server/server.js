3df035e763423f3e68b1ba665596397558e43c14
luigi055 <luigi5021@gmail.com>
2017-08-12 13:19:15 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({
+      todos,
+    });
+  }, err => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`successfully connected on port ${PORT}`);
 });
