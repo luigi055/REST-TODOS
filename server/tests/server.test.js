7d44a1270513511278d6e80ab3a834a23f88b7c5
luigi055 <luigi5021@gmail.com>
2017-08-12 20:51:46 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,19 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 const {
+  ObjectID
+} = require('mongodb');
+const {
   app
 } = require('./../server');
 const Todo = require('./../models/todo');
 
 // Dummy todos (SEED DATA)
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo',
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo',
 }]
 
@@ -71,25 +76,48 @@ describe('GET /todos', () => {
       .expect(res => {
         expect(res.body.todos).to.have.lengthOf(2);
       })
-      .end(done)
-    // .end((err, res) => {
-    //   if (err) return done(err);
+      .end((err, res) => {
+        if (err) return done(err);
 
-    //   Todo.find().then(todos => {
-    //     expect(todos).to.be.an('array').that.have.lengthOf(2);
-    //     done();
-    //   }).catch(err => done(err));
-    // });
+        Todo.find().then(todos => {
+          expect(todos).to.be.an('array').that.have.lengthOf(2);
+          done();
+        }).catch(err => done(err));
+      });
   });
 });
 
 describe('GET /todos/:todo', () => {
-  it('should get error if ID not found', done => {})
-  it('should get todo', done => {
-
+  it('should get todo document', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).to.be.equal(todos[0].text);
+      })
+      .end(done);
   })
+  it('should get error if ID not found', done => {
+    // create a new random id that cant be found
+    const hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .expect(res => {
+        expect(res.body.error).to.be.equal('Todo Not Found');
+      })
+      .end(done);
+
+  });
   it('should get error if ID is invalid', done => {
+    request(app)
+      .get(`/todos/123`)
+      .expect(404)
+      .expect(res => {
+        expect(res.body.error).to.be.equal('Invalid ID');
+      })
+      .end(done);
 
-  })
+  });
 
 });
